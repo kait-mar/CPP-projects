ed77797ae036cf8f415c9ca699fb612ee0383b91
Khaoula Ait maryam <kait-mar@s2r1p1.1337.ma>
2021-10-06 19:15:59 +0100

    starting day02

--- CPP02/ex00/Fixed.hpp
+++ CPP02/ex00/Fixed.hpp
@@ -4,6 +4,16 @@
 #include <iostream>
 #include <string>
 
-class
+class   Fixed
+{
+    int point_fix;
+    static const int    bit;
+    public:
+        Fixed() : point_fix(0);
+        ~Fixed();
+        Fixed(Fixed a) : point_fix(a.point_fix), bit(a.bit);
+        int getRawBits(void) const; 
+        void setRawBits(int const raw);
+};
 
 #endif
