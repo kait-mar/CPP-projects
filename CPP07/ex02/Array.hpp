e73de7a6617088fe3ee872dd00ef04ef7ac2f35e
Khaoula Ait maryam <kait-mar@e2r4p12.1337.ma>
2021-12-05 15:00:03 +0100

    d06 validated

--- CPP07/ex02/Array.hpp
+++ CPP07/ex02/Array.hpp
@@ -3,20 +3,88 @@
 
 #include <iostream>
 #include <string>
+#include <exception>
 
-template<typename T>
+class A{
+    public:
+    int value;
+};
+
+template<class T>
 class Array
 {
-    T   *array;
+    int len;
 public:
     Array();
     Array(unsigned int);
     Array(Array const &);
     void   operator=(Array const &);
-    T   &operator[](int);
+    T   &operator[](int) const;
     int size();
     ~Array();
+    T   *array;
 };
 
+template<class T>
+Array<T>::Array()
+{
+    this->array = NULL;
+    this->len = 0;
+}
+template<class T>
+Array<T>::Array(unsigned int n)
+{
+    this->array = new T[n]();
+    this->len = n;
+}
+template<class T>
+Array<T>::Array(Array const &obj)
+{
+    if (obj.array == NULL)
+        this->array = NULL;
+    else
+    {
+        this->len = obj.len;
+        this->array = new int[obj.len];
+        for (int i =0; i < obj.len; i++)
+            this->array[i] = obj[i];
+    }
+}
 
+template<class T>
+void    Array<T>::operator=(Array const &obj)
+{
+    if (obj.array == NULL)
+        this->array = NULL;
+    else
+    {
+        this->len = obj.len;
+        if (this->array != NULL)
+            delete [] this->array;
+        this->array = new int[obj.len];
+        for (int i =0; i < obj.len; i++)
+            this->array[i] = obj[i];
+    }
+}
+
+template <class T>
+T   &Array<T>::operator[](int idx) const
+{
+    if (idx < 0 || idx >= this->len)
+        throw (std::out_of_range("error"));
+    return (this->array[idx]);
+}
+
+template <class T>
+int Array<T>::size()
+{
+    return (this->len);
+}
+
+template <class T>
+Array<T>::~Array()
+{
+    if (this->array != NULL)
+        delete[] this->array;
+}
 #endif
--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -2,5 +2,22 @@
 
 int main()
 {
+    try
+    {
+        Array<int> a;
+        std::cout<<"a's size = "<< a.size() << std::endl;
+        Array<int> b(5);
+        std::cout<<"b's size = " << b.size() << std::endl;
+        std::cout<< "its first element = " << *(b.array) << std::endl;
+        Array<int> c(b);
+        a = c;
+       std::cout<<"c[3] = " << c[3]<< std::endl;
+      std::cout<<"c[52] = " << c[52]<< std::endl;
+       std::cout<<"c[0] = " << c[0]<< std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cout<< "[out of Range]\n";
+    }
     return (0);
 }
