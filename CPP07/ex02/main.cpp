d29deda51618aa9e9859128f95607f69177da9d5
Khaoula Ait maryam <kait-mar@e2r4p12.1337.ma>
2021-12-05 18:33:53 +0100

    started d08

--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -8,12 +8,12 @@ int main()
         std::cout<<"a's size = "<< a.size() << std::endl;
         Array<int> b(5);
         std::cout<<"b's size = " << b.size() << std::endl;
-        std::cout<< "its first element = " << *(b.array) << std::endl;
         Array<int> c(b);
         a = c;
        std::cout<<"c[3] = " << c[3]<< std::endl;
-      std::cout<<"c[52] = " << c[52]<< std::endl;
+       //std::cout<<"c[52] = " << c[52]<< std::endl;
        std::cout<<"c[0] = " << c[0]<< std::endl;
+        std::cout<<"a's size = "<< a.size() << std::endl;
     }
     catch(const std::exception& e)
     {
