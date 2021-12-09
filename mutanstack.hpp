8e37df497263f62bd7ced3c3c3d57fb9d9347b84
Khaoula AIT MARYAM <56926087+kait-mar@users.noreply.github.com>
2021-12-09 12:21:48 +0100

    Add files via upload

---
+++ mutanstack.hpp
@@ -0,0 +1,64 @@
+#ifndef MUTANSTACK_H
+#define MUTANSTACK_H
+
+#include <iostream>
+#include <exception>
+#include <list>
+#include <vector>
+#include <algorithm>
+
+template <class T>
+class MutantStack: std::stack<T>
+{
+public:
+    class Iterator
+    {
+    public:
+        Iterator(): index(0){}
+        Iterator(MutantStack<T> m): _m(m), index(0){}
+        T   operator*() const
+        {
+            //get sure of all this !!
+            MutantStack<T> temp = _m;
+            for (int i = 0; i < _m.size() - index; i++)
+                temp.pop();
+            return &temp.top();
+        }
+        void    operator++() const{
+            index++;
+        }
+        ~Iterator(){}
+        MutantStack<T>  _m;
+        int             index;
+        T   *ref;
+    };
+
+    MutantStack(): Iterator(*this){}
+    ~MutantStack(){}
+    Iterator beging() const
+    {
+        MutantStack<T>  temp = *this;
+        Iterator    it;
+        while (!temp.empty())
+        {
+            it.ref = &this->top();
+            temp.pop();
+        }
+        return (it);
+    }
+    Iterator end() const
+    {
+        Iterator it;
+        it.ref = &this->top() + 1;
+        return (it);
+    }
+};
+
+// template <class T>
+// class MutanStack<T>::Iterator    MutantStack<T>::begin()
+// {
+
+// }
+
+
+#endif
